3b43c993ef5b829f6bddc2c1dde6a4fb27a2b76e
Andriy Diulin <adiul.softserveinc.com>
2022-07-17 23:11:31 +0300

    +1

--- hello_world.js
+++ hello_world.js
@@ -1,14 +1,14 @@
 looker.plugins.visualizations.add({
   create: function(element, config){
-    element.innerHTML = "<h1>vvvv Ready to render!</h1>";
+    element.innerHTML = "<h1>Ready to render!</h1>";
   },
   updateAsync: function(data, element, config, queryResponse, details, doneRendering){
-    var html = `<h1>Ready to render!</h1>
+    var html = `<h1>now Ready to render!</h1>
     <table>
     `;
 
     for(var row of data) {
-      var cell = row[queryResponse.fields.dimensions[0].name] + 1;
+      var cell = row[queryResponse.fields.dimensions[0].name];
       html += "<tr><td>" + LookerCharts.Utils.htmlForCell(cell) + "</td></tr>";
     }
     html += "</table>";
